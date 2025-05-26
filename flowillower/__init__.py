19b31bb31720ae99f34dcce8614db415a70097d8
YeCanming <ycm24@mails.tsinghua.edu.cn>
2025-05-26 22:31:54 +0800

    refrac: nucleus

--- flowillower/__init__.py
+++ flowillower/__init__.py
@@ -4,4 +4,20 @@ import lazy_loader
 
 __getattr__, __dir__, __all__ = lazy_loader.attach_stub(__name__, __file__)
 
-__all__ = ["core", "foo"]
+__all__ = [
+    "author_name_en_us",
+    "author_name_zh_cn",
+    "core",
+    "foo",
+    "github_repo",
+    "github_user",
+    "help",
+    "import_name",
+    "lib_name",
+    "lib_name_en_us",
+    "lib_name_zh_cn",
+    "lib_paths",
+    "nucleus",
+    "pretty_name",
+    "upgrade_command_pip",
+]
--- flowillower/__init__.pyi
+++ flowillower/__init__.pyi
@@ -1,7 +1,38 @@
 from . import core
+from . import help
+from . import nucleus
 
 from .core import (
     foo,
 )
+from .help import (
+    author_name_en_us,
+    author_name_zh_cn,
+    github_repo,
+    github_user,
+    import_name,
+    lib_name,
+    lib_name_en_us,
+    lib_name_zh_cn,
+    lib_paths,
+    pretty_name,
+    upgrade_command_pip,
+)
 
-__all__ = ["core", "foo"]
+__all__ = [
+    "author_name_en_us",
+    "author_name_zh_cn",
+    "core",
+    "foo",
+    "github_repo",
+    "github_user",
+    "help",
+    "import_name",
+    "lib_name",
+    "lib_name_en_us",
+    "lib_name_zh_cn",
+    "lib_paths",
+    "nucleus",
+    "pretty_name",
+    "upgrade_command_pip",
+]
